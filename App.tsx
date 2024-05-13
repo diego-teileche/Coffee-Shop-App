424b5055811fe5688dd94cec24d9dcc64fd6c120
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-05-13 17:44:23 -0300

    Navigation functionality and Tab added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,37 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DetailsScreen from './src/screens/DetailsScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+import TabNavigator from './src/navigators/TabNavigator';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}
+        />
+
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{animation: 'slide_from_bottom'}}
+        />
+
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{animation: 'slide_from_bottom'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import CartScreen from '../screens/CartScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Favorite" component={FavoritesScreen} />
+      <Tab.Screen name="History" component={OrderHistoryScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TabNavigator;
