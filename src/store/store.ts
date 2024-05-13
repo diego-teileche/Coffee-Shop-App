295a93bb4ff3ce8848167474eb834d0eaeb87e7e
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-05-13 18:20:29 -0300

    Zustand store created

---
+++ src/store/store.ts
@@ -0,0 +1,20 @@
+import {create} from 'zustand';
+import {produce} from 'immer';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BeansData from '../data/BeansData';
+import CoffeeData from '../data/CoffeeData';
+
+export const useStore = create(
+  persist(
+    (set, get) => ({
+      CoffeeList: CoffeeData,
+      BeansList: BeansData,
+      CartPrice: 0,
+      FavoritesList: [],
+      CartList: [],
+      OrderHistoryList: [],
+    }),
+    {name: 'coffee-app', storage: createJSONStorage(() => AsyncStorage)},
+  ),
+);
