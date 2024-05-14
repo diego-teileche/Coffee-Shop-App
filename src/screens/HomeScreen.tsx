a8ab90504078836460495a588cad3bcb387a2f8e
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-05-14 18:34:50 -0300

    Data logic for HomeScreen created

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,52 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+
+const getCategoriesFromData = (data: any) => {
+  let temp: any = {};
+
+  for (let i = 0; i < data.length; i++) {
+    if (temp[data[i].name] === undefined) {
+      temp[data[i].name] = 1;
+    } else {
+      temp[data[i].name]++;
+    }
+
+    let categories = Object.keys(temp);
+    categories.unshift('All');
+
+    return categories;
+  }
+};
+
+const getCoffeeList = (category: string, data: any) => {
+  if (category === 'All') {
+    return data;
+  } else {
+    let coffeeList = data.filter((item: any) => item.name === category);
+
+    return coffeeList;
+  }
+};
 
 const HomeScreen = () => {
+  const CoffeeList = useStore((state: any) => state.CoffeeList);
+  const BeansList = useStore((state: any) => state.BeansList);
+  const [categories, setCategories] = useState(
+    getCategoriesFromData(CoffeeList),
+  );
+  const [searchText, setSearchText] = useState();
+  const [categoryIndex, setCategoryIndex] = useState({
+    index: 0,
+    category: categories[0],
+  });
+  const [sortedCoffee, setsortedCoffee] = useState(
+    getCoffeeList(categoryIndex.category, CoffeeList),
+  );
+
+  const tabBarHeight = useBottomTabBarHeight();
+
   return (
     <View>
       <Text>HomeScreen</Text>
