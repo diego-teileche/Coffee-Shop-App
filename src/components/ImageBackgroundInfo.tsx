bed215f6211376705cd38365fdbc7d4a05e6a9a7
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-05-23 17:56:50 -0300

    DetailsScreen | Details on image background added

--- src/components/ImageBackgroundInfo.tsx
+++ src/components/ImageBackgroundInfo.tsx
@@ -8,7 +8,14 @@ import {
 } from 'react-native';
 import React from 'react';
 import GradientBGIcon from './GradientBGIcon';
-import {COLORS, FONTSIZE, SPACING} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './CustomIcon';
 
 interface ImageBackgroundInfoProps {
   EnableBackHandler: boolean;
@@ -80,6 +87,69 @@ const ImageBackgroundInfo: React.FC<ImageBackgroundInfoProps> = ({
             </TouchableOpacity>
           </View>
         )}
+
+        <View style={styles.imageInfoOuterContainer}>
+          <View style={styles.imageInfoInnerContainer}>
+            <View style={styles.infoContainer}>
+              <View>
+                <Text style={styles.itemTitleText}>{name}</Text>
+                <Text style={styles.itemSubtitleText}>
+                  {special_ingredient}
+                </Text>
+              </View>
+
+              <View style={styles.itemPropertiesContainer}>
+                <View style={styles.ProperFirst}>
+                  <CustomIcon
+                    name={type == 'Beans' ? 'bean' : 'beans'}
+                    size={type == 'Beans' ? FONTSIZE.size_18 : FONTSIZE.size_24}
+                    color={COLORS.primaryOrangeHex}
+                  />
+
+                  <Text
+                    style={[
+                      styles.propertyTextFirst,
+                      {
+                        marginTop:
+                          type == 'Beans'
+                            ? SPACING.space_4 + SPACING.space_2
+                            : 0,
+                      },
+                    ]}>
+                    {type}
+                  </Text>
+                </View>
+
+                <View style={styles.ProperFirst}>
+                  <CustomIcon
+                    name={type == 'Beans' ? 'location' : 'drop'}
+                    size={FONTSIZE.size_16}
+                    color={COLORS.primaryOrangeHex}
+                  />
+
+                  <Text style={styles.propertyTextLast}>{ingredients}</Text>
+                </View>
+              </View>
+            </View>
+
+            <View style={styles.infoContainer}>
+              <View style={styles.ratingContainer}>
+                <CustomIcon
+                  name="star"
+                  color={COLORS.primaryOrangeHex}
+                  size={FONTSIZE.size_20}
+                />
+
+                <Text style={styles.ratingText}>{average_rating}</Text>
+                <Text style={styles.ratingCountText}>({ratings_count})</Text>
+              </View>
+
+              <View style={styles.roastedContainer}>
+                <Text style={styles.roastedText}>{roasted}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -102,6 +172,84 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-end',
+  },
+  imageInfoOuterContainer: {
+    paddingVertical: SPACING.space_24,
+    paddingHorizontal: SPACING.space_30,
+    backgroundColor: COLORS.primaryBlackRGBA,
+    borderTopLeftRadius: BORDERRADIUS.radius_20 * 2,
+    borderTopRightRadius: BORDERRADIUS.radius_20 * 2,
+  },
+  imageInfoInnerContainer: {
+    justifyContent: 'space-between',
+    gap: SPACING.space_15,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemTitleText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.primaryWhiteHex,
+  },
+  itemSubtitleText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  itemPropertiesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.space_20,
+  },
+  ProperFirst: {
+    height: 55,
+    width: 55,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  propertyTextFirst: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+  },
+  propertyTextLast: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+    marginTop: SPACING.space_2 + SPACING.space_4,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_10,
+    alignItems: 'center',
+  },
+  ratingText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+  ratingCountText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.primaryWhiteHex,
+  },
+  roastedContainer: {
+    height: 55,
+    width: 55 * 2 + SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  roastedText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
   },
 });
 
