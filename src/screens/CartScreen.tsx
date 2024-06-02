0fa934ac069aa4d5c965a0e46512f137dec37b6e
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-06-02 19:21:48 -0300

    CartScreen | Header and background color added

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,18 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useStore} from '../store/store';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import {COLORS} from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
 
 const CartScreen = () => {
   const cartList = useStore((state: any) => state.CartList);
-  console.log('cartList: ', cartList.length);
+  const cartPrice = useStore((state: any) => state.CartPrice);
+  const incrementCartItemQuantity = useStore(
+    (state: any) => state.incrementCartItemQuantity,
+  );
+  const decrementCartItemQuantity = useStore(
+    (state: any) => state.decrementCartItemQuantity,
+  );
+  const calculateCartPrice = useStore((state: any) => state.calculateCartPrice);
+
+  const tabBarHeight = useBottomTabBarHeight();
 
   return (
-    <View>
-      <Text>CartScreen</Text>
+    <View style={styles.screenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewFlex}>
+        <HeaderBar title="Cart" />
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  scrollViewFlex: {
+    flexGrow: 1,
+  },
+});
 
 export default CartScreen;
