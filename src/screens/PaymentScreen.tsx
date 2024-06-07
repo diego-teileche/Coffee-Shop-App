f5ce0baa365be7c0de0b7ae99f84fe35b46c6d0e
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-06-06 22:33:08 -0300

    PaymentScreen | Set up done

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,6 +1,29 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+const PaymentList = [
+  {
+    name: 'Wallet',
+    icon: 'icon',
+    isIcon: true,
+  },
+  {
+    name: 'Google Pay',
+    icon: require('../assets/app_images/gpay.png'),
+    isIcon: false,
+  },
+  {
+    name: 'Apple Pay',
+    icon: require('../assets/app_images/applepay.png'),
+    isIcon: false,
+  },
+  {
+    name: 'Amazon Pay',
+    icon: require('../assets/app_images/amazonpay.png'),
+    isIcon: false,
+  },
+];
+
 const PaymentScreen = () => {
   return (
     <View>
