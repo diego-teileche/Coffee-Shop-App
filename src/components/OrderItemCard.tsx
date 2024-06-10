aaafbeb4ddb7640b11be6643f7121c42629376db
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-06-10 18:46:26 -0300

    OrderHistoryScreen | HistoryCard - Item card - Image, title, subtitle and price added

--- src/components/OrderItemCard.tsx
+++ src/components/OrderItemCard.tsx
@@ -1,5 +1,13 @@
-import {ImageProps, StyleSheet, Text, View} from 'react-native';
+import {Image, ImageProps, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 
 interface OrderItemCardProps {
   type: string;
@@ -19,12 +27,69 @@ const OrderItemCard: React.FC<OrderItemCardProps> = ({
   itemPrice,
 }) => {
   return (
-    <View>
-      <Text>OrderItemCard</Text>
-    </View>
+    <LinearGradient
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
+      style={styles.cardLinearGradient}>
+      <View style={styles.cardInfoContainer}>
+        <View style={styles.cardImageInfoContainer}>
+          <Image source={imagelink_square} style={styles.image} />
+          <View>
+            <Text style={styles.cardTitle}>{name}</Text>
+            <Text style={styles.cardSubtitle}>{special_ingredient}</Text>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.cardCurrency}>
+            $ <Text style={styles.cardPrice}>{itemPrice}</Text>
+          </Text>
+        </View>
+      </View>
+    </LinearGradient>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardLinearGradient: {
+    gap: SPACING.space_20,
+    padding: SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  cardInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  cardImageInfoContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_20,
+    alignItems: 'center',
+  },
+  image: {
+    height: 80,
+    width: 80,
+    borderRadius: BORDERRADIUS.radius_15,
+  },
+  cardTitle: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryWhiteHex,
+  },
+  cardSubtitle: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_12,
+    color: COLORS.secondaryLightGreyHex,
+  },
+  cardCurrency: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_20,
+    color: COLORS.primaryOrangeHex,
+  },
+  cardPrice: {
+    color: COLORS.primaryWhiteHex,
+  },
+});
 
 export default OrderItemCard;
